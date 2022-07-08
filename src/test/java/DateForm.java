d56a81e933424d33d949a41689995506bd817362
MonokiT <timi_monoki@yahoo.com>
2022-07-08 22:30:22 +0300

    Find solution for HTML5 <input type="datetime-local"> element

--- src/test/java/DateForm.java
+++ src/test/java/DateForm.java
@@ -1,15 +1,7 @@
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.util.List;
 
 public class DateForm {
     public static void main(String[] args) {
@@ -33,28 +25,30 @@ public class DateForm {
             System.out.println(date.getText());
 
             WebElement newTimeDate =  driver.findElement(By.id("date-time-picker"));
+
             newTimeDate.clear();
-            newTimeDate.sendKeys("07-14-2022 14:10 ");
+            newTimeDate.sendKeys("07142022");
+            newTimeDate.sendKeys(Keys.TAB);
+            newTimeDate.sendKeys("1515PM");
 
             WebElement email =  driver.findElement(By.id("email-field"));
             email.sendKeys("marcel.pista966@e-uvt.ro");
 
             WebElement newMonth =  driver.findElement(By.id("month-field"));
-            newMonth.sendKeys("07-2022");
+
+            newMonth.sendKeys("07");
+            newMonth.sendKeys(Keys.TAB);
+            newMonth.sendKeys("2023");
 
             WebElement newNumber =  driver.findElement(By.id("number-field"));
             newNumber.clear();
             newNumber.sendKeys("52");
+            newNumber.sendKeys(Keys.UP);
 
             form.submit();
-
-
-//
-
-
-
         } catch (Exception | Error e) {
-            System.out.println("Am intrat in catch ");;
+            System.out.println("Am intrat in catch ");
+            System.out.println(e);
         } finally {
             if (driver != null) {
                 driver.quit();
